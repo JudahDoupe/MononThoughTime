9abecb99809ed4f5d0cc25b8f98b8a7bc9558c5a
Andrew Jackson <andrew.c.j1995@gmail.com>
2022-07-16 00:21:04 -0400

    Add danger chunk

--- Assets/Dimension.cs
+++ Assets/Dimension.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+// using System.Random;
+using Random = UnityEngine.Random;
 
 public class Dimension : MonoBehaviour
 {
   public List<GameObject> chunkPrefab;
+  public List<GameObject> dangerChunkPrefab;
   public int maxChunks;
   public int spawnZ => (maxChunks - 1)*15;
   public float chunkSpeed;
@@ -16,8 +19,7 @@ public class Dimension : MonoBehaviour
   {
     for (int i = 0; i < maxChunks; i++)
     {
-      activeChunks.Enqueue(Instantiate(chunkPrefab[0]));
-
+      spawnChunk();
     }
 
     var spaces = 0;
@@ -29,13 +31,25 @@ public class Dimension : MonoBehaviour
     }
   }
 
+  private GameObject spawnChunk()
+  {
+
+
+    var chunkType = Random.Range(-1, 1);
+    print(chunkType.ToString());
+    activeChunks.Enqueue(chunkType == 0
+      ? Instantiate(chunkPrefab[0], transform)
+      : Instantiate(dangerChunkPrefab[0], transform));
+
+    return activeChunks.Last();
+  }
+  
   private void Update()
   {
     if (activeChunks.Count < maxChunks && activeChunks.Last().transform.position.z <= spawnZ)
     {
-      var chunk_1 = Instantiate(chunkPrefab[0]);
+      var chunk_1 = spawnChunk();
       chunk_1.transform.position = new Vector3(0, 0, spawnZ);
-      activeChunks.Enqueue(chunk_1);
     }
 
 
@@ -49,4 +63,6 @@ public class Dimension : MonoBehaviour
       Destroy(activeChunks.Dequeue());
     }
   }
+
+
 }

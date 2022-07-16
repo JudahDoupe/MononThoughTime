b263b490a3435cea0993840b5e36a995826550c6
Ryan Clarke <ryan@ryanclarke.net>
2022-07-16 13:38:04 -0400

    Refactor Dimension spawning

--- Assets/Scripts/Dimension.cs
+++ Assets/Scripts/Dimension.cs
@@ -10,59 +10,50 @@ public class Dimension : MonoBehaviour
   public List<GameObject> chunkPrefab;
   public List<GameObject> dangerChunkPrefab;
   public int maxChunks;
-  public int spawnZ => (maxChunks - 1)*15;
   public float chunkSpeed;
   public Queue<GameObject> activeChunks = new();
   public float despawnThreshold;
+  public float chunkSize = 15;
 
-  public void Start()
+  private Vector3 NextSpawnPosition() => new(0, 0, (activeChunks.LastOrDefault()?.transform.localPosition.z ?? -chunkSize) + chunkSize);
+
+  private void spawnChunk()
   {
-    for (int i = 0; i < maxChunks; i++)
-    {
-      spawnChunk();
-    }
-
-    var spaces = 0;
-
-    foreach (var chunk in activeChunks)
-    {
-      chunk.transform.localPosition = new Vector3(0, 0, spaces);
-      spaces += 15;
-    }
-  }
-
-  private GameObject spawnChunk()
-  {
-
-
     var chunkType = Random.Range(-1, 1);
     print(chunkType.ToString());
-    activeChunks.Enqueue(chunkType == 0
+    
+    var chunk = chunkType == 0
       ? Instantiate(chunkPrefab[0], transform)
-      : Instantiate(dangerChunkPrefab[0], transform));
-
-    return activeChunks.Last();
+      : Instantiate(dangerChunkPrefab[0], transform);
+    chunk.transform.localPosition = NextSpawnPosition();
+    
+    activeChunks.Enqueue(chunk);
   }
   
   private void Update()
   {
-    if (activeChunks.Count < maxChunks && activeChunks.Last().transform.localPosition.z <= spawnZ)
+    while (activeChunks.Count < maxChunks)
     {
-      var chunk_1 = spawnChunk();
-      chunk_1.transform.localPosition = new Vector3(0, 0, spawnZ);
+      spawnChunk();
     }
-
-
+    
+    if (activeChunks.Count > maxChunks)
+    {
+      foreach (var extraChunk in activeChunks.Skip(maxChunks))
+      {
+        Destroy(extraChunk);
+      }
+      activeChunks = new Queue<GameObject>(activeChunks.Take(maxChunks));
+    }
+    
     foreach (var chunk in activeChunks)
     {
-      chunk.transform.localPosition = new Vector3(0, 0, chunk.transform.position.z - Time.deltaTime * chunkSpeed);
+      chunk.transform.localPosition = new Vector3(0, 0, chunk.transform.localPosition.z - Time.deltaTime * chunkSpeed);
     }
 
-    if (activeChunks.First().transform.localPosition.z <= despawnThreshold)
+    if (activeChunks.Peek().transform.localPosition.z <= despawnThreshold)
     {
       Destroy(activeChunks.Dequeue());
     }
   }
-
-
 }
